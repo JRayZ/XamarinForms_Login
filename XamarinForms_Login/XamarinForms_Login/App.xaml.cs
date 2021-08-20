0f506ffb6efee7232c40450b63dfb9af7990dc31
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-08-20 15:28:16 -0400

    Changing MainPage

--- XamarinForms_Login/XamarinForms_Login/App.xaml.cs
+++ XamarinForms_Login/XamarinForms_Login/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinForms_Login.View;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace XamarinForms_Login
 {
     public partial class App : Application
@@ -10,7 +12,7 @@ namespace XamarinForms_Login
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LogIn());
         }
 
         protected override void OnStart()
