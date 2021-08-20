e5f417c86d54e64d00d1e9f91d8c84e835eea983
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-08-20 15:48:57 -0400

    LogInView - validate the Email and Password.

---
+++ XamarinForms_Login/XamarinForms_Login/ViewModel/LogInView.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinForms_Login.View;
+
+namespace XamarinForms_Login.ViewModel
+{
+    public class LogInView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public LogInView()
+        {
+
+        }
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+        public Command LogInCommand
+        {
+            get
+            {
+                return new Command(Login);
+            }
+        }
+        
+        private void Login()
+        {
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
+                App.Current.MainPage.DisplayAlert("Empty Values", "Please enter Email and Password", "OK");
+            else
+            {
+                if (Email == "abc@gmail.com" && Password == "1234")
+                {
+                    App.Current.MainPage.DisplayAlert("Login Success", "", "Ok");
+                    App.Current.MainPage.Navigation.PushAsync(new Welcome());
+                }
+                else
+                    App.Current.MainPage.DisplayAlert("Login Fail", "Please enter correct Email and Password", "OK");
+            }
+        }
+    }
+}
