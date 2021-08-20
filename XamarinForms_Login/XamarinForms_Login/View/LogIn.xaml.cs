1b01a30f4e4130b399303b93bc3018a2f3576e65
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-08-20 16:31:25 -0400

    Show alert messages

--- XamarinForms_Login/XamarinForms_Login/View/LogIn.xaml.cs
+++ XamarinForms_Login/XamarinForms_Login/View/LogIn.xaml.cs
@@ -1,39 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinForms_Login.ViewModel;
 
 namespace XamarinForms_Login.View
-{
+{ 
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LogIn : ContentPage
     {
+        LogInView logInView;
         public LogIn()
         {
+            logInView = new LogInView();
             InitializeComponent();
+            BindingContext = logInView;
         }
-
-        private void LoginBtnClicked(object sender, EventArgs e)
-        {  
-            if (string.IsNullOrEmpty(Email.Text) || string.IsNullOrEmpty(Password.Text))
-                DisplayAlert("Empty Values", "Please enter Email and Password", "OK");
-            else
-            {
-                if (Email.Text == "jhonatan@gmail.com" && Password.Text == "abcde123")
-                {
-                    DisplayAlert("Login Success", "", "Ok");
-                    Navigation.PushAsync(new Welcome());
-                }
-                else
-                {
-                    DisplayAlert("Login Fail", "Please enter correct Email and Password", "OK");
-                }
-            }
-        }
-
     }
-}
+}  
